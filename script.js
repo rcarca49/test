78d04501523d6eaf3383e8cae8adac1fdf964e46
rcarca49 <rcarcarca494949@gmail.com>
2024-12-09 01:54:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('changeColorBtn').addEventListener('click', function() {
+  document.body.style.backgroundColor = document.body.style.backgroundColor === 'lightblue' ? '#f4f4f9' : 'lightblue';
+});
